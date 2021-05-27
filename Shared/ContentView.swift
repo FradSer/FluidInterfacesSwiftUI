fbc326e2ec0b1d03823b9244752c0e157f94d4e6
Frad LEE <fradser@gmail.com>
2021-05-27 20:06:35 +0800

    build(xcode): initialize project
    With .gitignore

---
+++ Shared/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Frad LEE on 5/27/21.
+//
+
+import SwiftUI
+
+// MARK: - ContentView
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+// MARK: - ContentView_Previews
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ Shared/FluidInterfacesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluidInterfacesSwiftUIApp.swift
+//  Shared
+//
+//  Created by Frad LEE on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct FluidInterfacesSwiftUIApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
