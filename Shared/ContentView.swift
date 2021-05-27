40f59441bf49d68da1965ce22645d947247838fa
Frad LEE <fradser@gmail.com>
2021-05-27 21:04:26 +0800

    feat(list): base navigation view ui
    With icons copy from https://github.com/nathangitter/fluid-interfaces

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,12 +7,54 @@
 
 import SwiftUI
 
+// MARK: - ListItem
+
+struct ListItem: Identifiable {
+  let id = UUID()
+  let icon: String
+  let name: String
+}
+
 // MARK: - ContentView
 
 struct ContentView: View {
+  let list = [
+    ListItem(icon: "icon_calc", name: "Calculator Button"),
+    ListItem(icon: "icon_spring", name: "Spring Animations"),
+    ListItem(icon: "icon_flash", name: "Flashlight Button"),
+    ListItem(icon: "icon_rubber", name: "Rubberbanding"),
+    ListItem(icon: "icon_acceleration", name: "Acceleration Pausing"),
+    ListItem(icon: "icon_momentum", name: "Rewarding Momentum"),
+    ListItem(icon: "icon_pip", name: "FaceTime PiP"),
+    ListItem(icon: "icon_rotation", name: "Rotation")
+  ]
+
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    NavigationView {
+      List(list) { listItem in
+        NavigationLink(destination: destinationView(listItem.name)) {
+          HStack {
+            Image(listItem.icon)
+              .resizable()
+              .frame(width: 28, height: 28, alignment: .center)
+            Text(listItem.name).bold()
+          }
+          .padding(.vertical)
+        }
+      }
+      .listStyle(InsetGroupedListStyle())
+      .navigationTitle("Fluid Interfaces")
+    }
+  }
+
+  // TODO: Create a view model with this function
+  @ViewBuilder func destinationView(_ destination: String) -> some View {
+    switch destination {
+    case "Calculator Button":
+      CalculatorButtonView()
+    default:
+      Text("ERROR")
+    }
   }
 }
 
@@ -21,5 +63,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+      .preferredColorScheme(.dark)
   }
 }
---
+++ Shared/Views/CalculatorButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorButtonView.swift
+//  FluidInterfacesSwiftUI
+//
+//  Created by lixi on 5/27/21.
+//
+
+import SwiftUI
+
+struct CalculatorButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculatorButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorButtonView()
+    }
+}
