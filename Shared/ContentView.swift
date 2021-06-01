bf36b3ed0bb0bceaf50131542455330b78e7ed44
Frad LEE <fradser@gmail.com>
2021-06-01 18:23:03 +0800

    feat(list): update ui
    Add `RubberbandingView`

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -56,6 +56,8 @@ struct ContentView: View {
       SpringAnimationsView()
     case "Flashlight Button":
       FlashlightButtonView()
+    case "Rubberbanding":
+      RubberbandingView()
     default:
       Text("ERROR")
     }
---
+++ Shared/Views/RubberbandingView.swift
@@ -0,0 +1,24 @@
+//
+//  RubberbandingView.swift
+//  FluidInterfacesSwiftUI
+//
+//  Created by lixi on 6/1/21.
+//
+
+import SwiftUI
+
+// MARK: - RubberbandingView
+
+struct RubberbandingView: View {
+  var body: some View {
+    Text("Rubberbanding View")
+  }
+}
+
+// MARK: - RubberbandingView_Previews
+
+struct RubberbandingView_Previews: PreviewProvider {
+  static var previews: some View {
+    RubberbandingView()
+  }
+}
